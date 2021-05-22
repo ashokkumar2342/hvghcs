6b498786eeb23f0c0ec1c7e339983af16ec1074f
ashokkumar2342 <ashokkumar2342@gmail.com>
2021-05-22 20:33:35 +0530

    Update list.blade.php

--- resources/views/admin/form3/list.blade.php
+++ resources/views/admin/form3/list.blade.php
@@ -13,7 +13,7 @@
 		<tr>
 			<td>{{ date('d-m-Y',strtotime($Form3->fordate)) }}</td>
 			<td>{{ $Form3->household_covered }}</td>
-			<td>{{ $Form3->household_covered }}</td>
+			<td>{{ $Form3->highrisk_household }}</td>
 			<td>
 			<a onclick="callPopupLarge(this,'{{ route('admin.form3.edit',$Form3->id) }}')" title="Edit" class="btn btn-xs btn-info"><i class="fa fa-edit"></i></a> 
 			<a  select-triger="village_select_box" success-popup="true" onclick="callAjax(this,'{{ route('admin.form3.delete',$Form3->id) }}')" title="Delete" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
